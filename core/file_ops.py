b91595f93d689ea6845a21c02a43bce8da948d0d
Anant Sharma <97672203+codelinechef@users.noreply.github.com>
2025-08-22 15:01:07 +0530

    Delete core/file_ops.py

--- core/file_ops.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def create_file(name, content = ""):
-    with open(name, 'w') as f:
-        f.write(content)
-
-def read_file(name):
-    with open(name, "r") as f:
-        return f.read()
